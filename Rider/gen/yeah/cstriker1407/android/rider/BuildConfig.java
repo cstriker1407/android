4d0910177d3bb256199b472a1cab27bc0eac78b4
cstriker1407 <cstriker1407@yeah.net>
2013-10-17 22:02:00 +0800

    init Rider Project

---
+++ Rider/gen/yeah/cstriker1407/android/rider/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package yeah.cstriker1407.android.rider;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Rider/src/yeah/cstriker1407/android/rider/WelcomeActivity.java
@@ -0,0 +1,7 @@
+package yeah.cstriker1407.android.rider;
+
+import android.app.Activity;
+
+public class WelcomeActivity extends Activity {
+
+}
