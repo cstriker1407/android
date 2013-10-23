96d744011e70aa791d40a61940f97576d06235ef
cstriker1407 <cstriker1407@yeah.net>
2013-10-23 23:55:53 +0800

    增加了各种progressbar的实现

--- HelloPorgressBar/gen/com/example/helloporgressbar/R.java
+++ HelloPorgressBar/gen/com/example/helloporgressbar/R.java
@@ -21,14 +21,35 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int background=0x7f020000;
+        public static final int circle_progress=0x7f020001;
+        public static final int circle_progress_rotate=0x7f020002;
+        public static final int color_progressbar=0x7f020003;
+        public static final int dialog_style_xml_color=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int picture=0x7f020006;
+        public static final int picture_progressbar=0x7f020007;
+        public static final int progress=0x7f020008;
+        public static final int sword_black=0x7f020009;
+        public static final int sword_color=0x7f02000a;
+        public static final int vertical=0x7f02000b;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
+        public static final int action_settings=0x7f08000e;
+        public static final int bar_color=0x7f080007;
         public static final int bar_horizontal=0x7f080003;
         public static final int bar_large=0x7f080000;
         public static final int bar_normal=0x7f080001;
         public static final int bar_small=0x7f080002;
+        public static final int btn_center=0x7f080006;
+        public static final int btn_left=0x7f080004;
+        public static final int btn_right=0x7f080005;
+        public static final int progressBar1=0x7f080008;
+        public static final int progressBar2=0x7f080009;
+        public static final int progressBar3=0x7f08000a;
+        public static final int progressBar4=0x7f08000b;
+        public static final int progressBar5=0x7f08000c;
+        public static final int progressBar6=0x7f08000d;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- HelloPorgressBar/src/com/example/helloporgressbar/MainActivity.java
+++ HelloPorgressBar/src/com/example/helloporgressbar/MainActivity.java
@@ -3,13 +3,51 @@ package com.example.helloporgressbar;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
+	
+	private ProgressBar bar_large;
+	private ProgressBar bar_horizontal;
+	private ProgressBar bar_color;
+	
+	
+	private Button btn_left;
+	private Button btn_center;
+	private Button btn_right;
+
+	
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		//标题栏上设置一个进度条
 		super.onCreate(savedInstanceState);
+		
+		requestWindowFeature(Window.FEATURE_PROGRESS);
+		
 		setContentView(R.layout.activity_main);
+		
+		bar_large = (ProgressBar)findViewById(R.id.bar_large);
+		bar_horizontal = (ProgressBar)findViewById(R.id.bar_horizontal);
+		bar_color = (ProgressBar)findViewById(R.id.bar_color);
+		
+		
+		btn_left = (Button)findViewById(R.id.btn_left);
+		btn_center = (Button)findViewById(R.id.btn_center);
+		btn_right = (Button)findViewById(R.id.btn_right);
+		btn_left.setOnClickListener(this);
+		btn_center.setOnClickListener(this);
+		btn_right.setOnClickListener(this);
+		
+		
+		
+		
 	}
 
 	@Override
@@ -17,6 +55,47 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) 
+	{
+		switch (v.getId())
+		{
+		case R.id.btn_left:
+		{
+			bar_large.setMax(100);
+			bar_large.setProgress(50);
+			bar_large.setSecondaryProgress(75);
+			
+			bar_horizontal.setMax(100);
+			bar_horizontal.setProgress(50);
+			bar_horizontal.setSecondaryProgress(75);
+			
+			bar_color.setMax(100);
+			bar_color.setProgress(50);
+			bar_color.setSecondaryProgress(75);
+			
+			
+			break;
+		}
+		case R.id.btn_center:
+		{
+			setProgressBarVisibility(true);
+			setProgress(5000);
+			break;
+		}
+
+		case R.id.btn_right:
+		{
+			break;
+		}
+
+		default:
+			break;
+		}
+		
+		
 	}
 
 }
